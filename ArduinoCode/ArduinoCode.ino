9877d1e8b911783c17d7a4c8e63570624d5b2fa4
trongczcom <trongcz.com@gmail.com>
2026-04-12 21:07:51 +0700

    Đạt. Test3_fixBG.py và Arduinocode

--- ArduinoCode/ArduinoCode.ino
+++ ArduinoCode/ArduinoCode.ino
@@ -19,8 +19,9 @@
 #define DC      4
 
 #define TO1     2000
-#define POS0    0
-#define POS1    90
+#define TO2     1000
+#define POS0    90
+#define POS1    40
 
 LiquidCrystal_I2C lcd(0x27,16, 2);
 
@@ -30,18 +31,21 @@ int servo_pin[4] = {5, 6, 7, 8};
 Servo servo[4];
 
 String s;
-unsigned long t1;
+unsigned long t1, t2;
 bool received = 0;
 bool programData = 0;
 bool programColor = 0;
 int colorX;
+bool wait1 = 0;
+bool wait2 = 0;
+
+bool onDC = 0;
 
 void setup()
 {
-  pinMode(CB1, INPUT);
-  pinMode(CB1, INPUT);
-  pinMode(CB1, INPUT);
-  pinMode(CB1, INPUT);
+  for(int i = 0; i<4; i++){
+    pinMode(sensor[i], INPUT);
+  }
   
   pinMode(BT1, INPUT_PULLUP);
   pinMode(BT2, INPUT_PULLUP);
@@ -49,31 +53,42 @@ void setup()
   
   pinMode(DC, OUTPUT);
 
-  digitalWrite(DC, HIGH);
+  digitalWrite(DC, LOW);
 
   Serial.begin(9600);
   Serial.println("Start...");
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(6,0);
-  lcd.print("Hi!");
-
-  for(int i=0; i<4; i++){
+  
+  for(int i = 0; i<4; i++){
     servo[i].attach(servo_pin[i]);
-    servo[i].write(POS0); 
+    servo[i].write(POS0);
   }
 
+  Serial.println("Dang kiem tra ...");
+  Test();
+  Serial.println("Kiem tra xong ...");
+  lcd.clear();
+  lcd.setCursor(6,0);
+  lcd.print("Hi!");
   delay(1000);
 }
 
 
 void loop()
 {
+  ControlDC();
   if (Serial.available()) {
     s = Serial.readStringUntil('\n');
-    received = 1;
-    programData = 1;
-    t1 = millis() + TO1;
+    //lcd.setCursor(14,1);
+    // lcd.print(programData);
+    // lcd.print(programColor);
+    if(!programColor){
+      received = 1;
+      programData = 1;
+      t1 = millis() + TO1;
+    }
+    
   }
 
   if(received){
@@ -87,6 +102,7 @@ void loop()
       // }
       //lcd.clear();
       LcdClear();
+      lcd.setCursor(0,1);
       lcd.print(s);
 
       for(int i=0; i<4; i++){
@@ -97,9 +113,8 @@ void loop()
           servo[colorX].write(POS1); 
         }
       }
-      programData = 0;
       programColor = 1;
-
+      programData = 0;
     }
     
     if(millis() > t1){
@@ -110,8 +125,18 @@ void loop()
   }
 
   if(programColor){
-    if(digitalRead(sensor[colorX]) == 0){
+    if(!wait1 && digitalRead(sensor[colorX]) == 0){
+      wait1 = 1;
+      t2 = millis() + TO1;
+    }
+    if(wait1 && millis()>t2){
       servo[colorX].write(POS0);
+      wait1 = 0;
+      wait2 = 1;
+      t2 = millis() + TO2;
+    }
+    if(wait2 && millis()>t2){
+      wait2 = 0;
       programColor = 0;
     }
   }
@@ -119,5 +144,42 @@ void loop()
 void LcdClear(){
   lcd.setCursor(0,1);
   for(int i = 0; i<16; i++)
-  lcd.print(" ");
+    lcd.print(" ");
 }
+void ControlDC(){
+  if(!digitalRead(BT1)){
+    if(onDC){
+      onDC = 0;
+      digitalWrite(DC, LOW);
+    } else {
+      onDC = 1;
+      digitalWrite(DC, HIGH);
+    }
+    while (!digitalRead(BT1));
+    delay(100);
+  }
+}
+void Test(){
+  lcd.clear();
+  lcd.print("Test..");
+  digitalWrite(DC, HIGH);
+  delay(500);
+  for(int i = 0; i<4; i++){
+    lcd.print(i);
+    servo[i].write(POS1);
+    delay(1000);
+    servo[i].write(POS0);
+    delay(200);
+  }
+
+  lcd.print(".");
+  for(int i = 0; i<4; i++){
+    while(digitalRead(sensor[i]));
+    lcd.print(i);  
+  }
+  digitalWrite(DC, LOW);
+  lcd.setCursor(0,1);
+  lcd.print("OK!");
+  delay(1000);
+}
+
